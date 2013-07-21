9c691af7ea221562052b1bd9f5da1743d515284d
OmarLS <dragons2113@hotmail.com>
2013-07-21 13:50:20 -0500

    actualiza2

--- PrograWeb/aplication/models/ManejoBaseDatos.php
+++ PrograWeb/aplication/models/ManejoBaseDatos.php
@@ -9,15 +9,34 @@ class ManejoBaseDatos {
     
     private $db;
     
+    
     function ManejoBaseDatos() {
         $this->db = ADONewConnection('mysql');
         $this->db->debug = true;
-        $this->db->Connect('localhost', 'root', '', 'sitec');
+        $this->db->Connect('localhost', 'root', '', 'ProyectoX');
         if(db){
             echo 'Chido';
         }else{
             echo $db;
         }
+        
+        $nombre_asistente = $_POST["nombre_asistente"];
+        $apellido_paterno = $_POST["apellido_paterno"];
+        $apellido_materno = $_POST["apellido_materno"];
+        $genero = $_POST["genero"];
+        $edad = $_POST["edad"];
+        $email = $_POST["email"];
+        $nctrl_rfc = $_POST["nctrl_rfc"];
+        $password = $_POST["password"];
+        
+        $actual = mysqli_query($enlace,"call actualiza('$nombre_asistente','$apellido_paterno',
+                                                        '$apellido_materno','$genero','$edad',
+                                                        ,'$email','$nctrl_rfc')");
+        if (!$actual){
+            echo "Error al guardar";
+            }
+            else{echo "Guardado con exito";}
+        mysqli_close($enlace);
     }   
     
 
